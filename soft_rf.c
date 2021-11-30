7a392642c0e2466fdc51ca230d16e62a516ae94a
user <user@DESKTOP-H84N6NL>
2021-11-30 21:38:04 +0300

    Добавьте файлы проекта.

---
+++ soft_rf.c
@@ -0,0 +1,132 @@
+#include"soft_rf.h"
+
+const uint32_t speed_[] = { 9600, 19200, 38400, 57600, 76800, 115200 };
+
+const uint8_t start_symbol = 0x38;
+
+bit_time init_timings_(uint32_t speed, uint32_t timer_freq)
+{
+	bit_time bt;
+	// длительность 1сек(10^9 мкс) бита 1/скорость связи
+	bt.bit_time_ = 1000000000.0 / speed;
+	// длительность 1сек(10^9 мкс) тика 1/частота
+	bt.one_timer_tick_time_ = 1000000000.0 / timer_freq;
+	bt.timer_ticks_per_bit_ = round(bt.bit_time_ / \
+		bt.one_timer_tick_time_);
+	// допуск на длину бита +- 1/50 длительности бита
+	bt.delta_timer_ticks_per_bit_ = bt.timer_ticks_per_bit_ / 50;
+	bt.timer_ticks_per_bit_min_ = bt.timer_ticks_per_bit_ - \
+		bt.delta_timer_ticks_per_bit_;
+	bt.timer_ticks_per_bit_max_ = bt.timer_ticks_per_bit_ + \
+		bt.delta_timer_ticks_per_bit_;
+	bt.start_bit_ticks_ = 3 * bt.timer_ticks_per_bit_;
+	bt.start_bit_ticks_min_ = bt.start_bit_ticks_ - \
+		bt.delta_timer_ticks_per_bit_;
+	bt.start_bit_ticks_max_ = bt.start_bit_ticks_ + \
+		bt.delta_timer_ticks_per_bit_;
+	return bt;
+}
+
+void Add_converted_signal_to_data(uint16_t* buffer, uint16_t* sequence, uint16_t* sequence_iterator)
+{
+
+}
+
+data_full_msg init_data_struct()
+{
+	data_full_msg data;
+	data.data_length_ = 0;
+	data.data_iterator_ = 0;
+	for (unsigned i = 0; i < MAX_DATA_LENGTH; i++) data.data_[i] = i;
+	data.CRC_message_[0] = 0;
+	data.CRC_message_[1] = 0;
+	return data;
+}
+
+
+
+// выполняется во время прерывания по запросу ПДП (DMA)
+uint8_t Add_signal_to_sequence(bit_time* bt, uint16_t* buffer, timer_receive_sequence *sequence, data_full_msg * data)
+{
+	if (sequence->sequence_iterator_ == MAX_SEQUENCE_ITERATOR) return 1;
+	sequence->receive_sequence_[sequence->sequence_iterator_] = buffer[0];
+	++sequence->sequence_iterator_;
+	sequence->receive_sequence_[sequence->sequence_iterator_] = buffer[1];
+	++sequence->sequence_iterator_;
+	Convert_sequence_find_data_length(bt, sequence, data);
+	if (sequence->sequence_iterator_ == MAX_SEQUENCE_ITERATOR) sequence->sequence_iterator_ = 0; // обнуление последовательности
+	// принимаю длину сообщения, устанавливаю sequence_iterator в 0, принимаую сообщение
+	// , при достижении sequence iterator длины сообщения снимаю флаг start_bit_ вызываю прерывание?!?!?
+	return 0;
+}
+
+// поиск начала сообщение и его расшифровка
+// разворот битов из LSB в MSB
+uint8_t Convert_sequence_find_data_length(bit_time* bt, timer_receive_sequence* tim_seq, data_full_msg* data )
+{
+	static uint8_t start_ = 0;
+	static uint16_t buffer_ = { 0, };  // буфер для заполнения 6 битными значениями
+	static uint8_t buffer_iterator_ = 0;
+	static uint8_t word_buffer_ = 0; // заполняется по 4 бита, для учета нужен итератор
+	static uint8_t halfword_iterator_ = 0;
+	uint8_t count_1 = 0, count_0 = 0;
+	if (start_ == tim_seq->sequence_iterator_)return 0;
+	// поиск
+	for (uint8_t i = start_; i < tim_seq->sequence_iterator_; i = i + 2) {//TODO: что если sequence_iterator нечетный
+
+		// разделяю на длительность бита (в отсчетах таймера)
+		count_1 = tim_seq->receive_sequence_[i] / bt->timer_ticks_per_bit_;  // TODO: 16 BIT TO 8 BIT
+		if (tim_seq->receive_sequence_[i] % bt->timer_ticks_per_bit_ > \
+			bt->timer_ticks_per_bit_min_ * count_1) ++count_1; // проверяю остаток
+		count_0 = tim_seq->receive_sequence_[i + 1] / bt->timer_ticks_per_bit_;
+		if (tim_seq->receive_sequence_[i + 1] % bt->timer_ticks_per_bit_ > \
+			bt->timer_ticks_per_bit_min_ * count_0) ++count_0;
+
+		// перевожу из "1" и "0" в слово
+		// считаю что данные приходят младшим битом вперед
+		for (uint8_t y = 0; y < count_1; ++y) {
+			buffer_ |= 1 << buffer_iterator_;
+			++buffer_iterator_; // до этого момента buffer_iterator не должен быть больше 6
+			if (buffer_iterator_ == 6) {
+				if (halfword_iterator_ == 0) {
+					word_buffer_ = 0;
+					word_buffer_ |= Convert_6to4(buffer_);
+				}
+				else {
+					word_buffer_ |= Convert_6to4(buffer_) << 4;
+					halfword_iterator_ = 0;
+					// TODO: 8 bit received
+				}
+				buffer_ = 0;
+			}
+		}
+		if (buffer_iterator_ + count_0 >= 6) {
+			if (halfword_iterator_ == 0) {
+				word_buffer_ = Convert_6to4(buffer_);
+			}
+			else {
+				word_buffer_ = Convert_6to4(buffer_) << 4;
+				halfword_iterator_ = 0;
+				// TODO: 8 bit received
+			}
+			buffer_iterator_ = count_0 - (6 - buffer_iterator_);
+		}
+		buffer_iterator_ += count_0;
+	}
+}
+
+uint8_t Convert_6to4(uint8_t data_6bit_in)
+{
+	for (uint8_t i = 0; i < 16; ++i) {
+		if (data_6bit_in == symbols[i])return i;
+	}
+	return 0;
+}
+
+uint8_t Convert_4to6(uint8_t data_4bit_in)
+{
+	return symbols[data_4bit_in];
+}
+
+
+
---
+++ soft_rf.h
@@ -0,0 +1,102 @@
+/*
+ * soft_rf.h
+ *
+ *  Created on: 14 апр. 2021 г.
+ *      Author: sergey
+ * 
+ * Техническое задание
+ * 
+ * Входящой сигнал - последовательность количества тиков таймера
+ * Выходной сигнал - ШИМ (изменение значение регистра CCR таймера для посредством DMA)
+ * 
+ * преобразование 4 to 6 и обратно необходимо для баланса 1 и 0 в сигнале (пока по таблице ~30% скорости)
+ * CRC аппаратное
+ * DMA + PWM для передачи
+ * DMA + TIM для приема
+ * 
+ * 
+ * 
+ *      Преобразование входного сигнала в сообщение:
+ *          1) перевод количество тиков таймера в 1 и 0
+ *          2) перевод 6to4 
+ *          3) соединение 4 + 4  или другие
+ *          5) проверка CRC
+ * 
+ *      Преобразование сообщения в данные:
+ *          1) расчет CRC
+ *          2) разбиение на 4 бита
+ *          3) перевод 4to6
+ *          4) перевод в длительности ШИМ 
+ *
+ * 
+ */
+
+#ifndef INC_SOFT_RF_H_
+#define INC_SOFT_RF_H_
+
+#include<math.h>
+#include<inttypes.h>
+
+#define BIT_PER_SECOND_TRANSFER_SPEED 4 // speed rate in bts speed_[] bellow
+
+ //const uint16_t max_sequence_iterator_ = 24; // 12 бит Х 2
+#define MAX_SEQUENCE_ITERATOR 24
+//const uint8_t max_data_length_ = 27; // 27 byte data + 1 byte data lenght + 2 byte CRC
+#define MAX_DATA_LENGTH 27
+
+#define TIMER_CLOCK_FREQ 72000000;
+
+typedef struct
+{
+     double bit_time_; // длительность 1сек(10^9 мкс) бита 1/скорость связи
+     double one_timer_tick_time_ ; // длительность 1сек(10^9 мкс) тика 1/частота
+     uint32_t timer_ticks_per_bit_;
+     uint8_t delta_timer_ticks_per_bit_ ;  // допуск на длину бита +- 1/50 длительности бита
+     uint16_t timer_ticks_per_bit_min_;
+     uint16_t timer_ticks_per_bit_max_;
+     uint16_t start_bit_ticks_;
+     uint16_t start_bit_ticks_min_;
+     uint16_t start_bit_ticks_max_;
+ }bit_time;
+
+extern const uint32_t speed_[];
+
+extern const uint8_t start_symbol; // 0x38 0b111000  12bit symbol <--- 2x
+
+static const uint8_t symbols[] =    // спизжено с virtualwire - данная таблица переводит 4 бита в 6, при этом у 6 бит равное отношение "1" и "0" т.е. 3 "1" и 3 "0"
+{
+    0xd,  0xe,  0x13, 0x15, 0x16, 0x19, 0x1a, 0x1c,
+    0x23, 0x25, 0x26, 0x29, 0x2a, 0x2c, 0x32, 0x34
+};
+
+typedef struct
+{
+    // циклический буффер значений таймера
+    uint16_t receive_sequence_[MAX_SEQUENCE_ITERATOR]; //{0,} // принимаемая последовательность
+// (не переведенная) (304 бита (19*16бит) округл до 20)
+    uint16_t sequence_iterator_; // 0
+}timer_receive_sequence;
+
+
+typedef struct
+{
+    uint8_t data_length_; // = 0; 
+    uint16_t data_iterator_;//  0; // побитовый итератор, номера битов: 7`6`5`4`3`2`1`0  15`14`13`12`11`10`9`8   23`22`21 ...
+    uint8_t data_[MAX_DATA_LENGTH];//{ 0, };
+    uint8_t CRC_message_[2]; // { 0, };
+} data_full_msg;
+
+// оцифровывает добавляет сигнал в виде двух длительностей
+//  ( длительность выского уровня и длительность низкого уровня) к
+// последовательности данных итератор указывает на позицию вставки следующего значения
+
+uint8_t init_rf(); // init radio 
+bit_time init_timings_(uint32_t speed, uint32_t timer_freq);
+data_full_msg init_data_struct();
+void Add_converted_signal_to_data(uint16_t *buffer, timer_receive_sequence *sequence, data_full_msg* data); // buffer - place where are dma send data_, sequence -... this function
+uint8_t Add_signal_to_sequence(bit_time* bt, uint16_t* buffer, timer_receive_sequence* sequence, data_full_msg* data);
+uint8_t Convert_sequence_find_data_length(bit_time* bt, timer_receive_sequence* timers_sequence, data_full_msg* data);
+uint8_t Convert_6to4(uint8_t data_6bit_in); // return converted 4 bit , just "inline" it
+uint8_t Convert_4to6(uint8_t data_4bit_in); // return converted 6 bit ,
+
+#endif /* INC_SOFT_RF_H_ */
