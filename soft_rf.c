f80cf6092cc3ac247ba9b7b0b3616f5a29998a72
seregray <sochnevs@list.ru>
2021-12-04 06:48:19 +0300

    Convert_sequence_find_data_length refactoring

--- soft_rf.c
+++ soft_rf.c
@@ -11,15 +11,15 @@ bit_time init_timings_(uint32_t speed, uint32_t timer_freq)
 	bt.bit_time_ = 1000000000.0 / speed;
 	// длительность 1сек(10^9 мкс) тика 1/частота
 	bt.one_timer_tick_time_ = 1000000000.0 / timer_freq;
-	bt.timer_ticks_per_bit_ = round(bt.bit_time_ / \
+	bt.TIM_ticks_per_bit_ = round(bt.bit_time_ / \
 		bt.one_timer_tick_time_);
 	// допуск на длину бита +- 1/50 длительности бита
-	bt.delta_timer_ticks_per_bit_ = bt.timer_ticks_per_bit_ / 50;
-	bt.timer_ticks_per_bit_min_ = bt.timer_ticks_per_bit_ - \
+	bt.delta_timer_ticks_per_bit_ = bt.TIM_ticks_per_bit_ / 50;
+	bt.TIM_ticks_per_bit_min_ = bt.TIM_ticks_per_bit_ - \
 		bt.delta_timer_ticks_per_bit_;
-	bt.timer_ticks_per_bit_max_ = bt.timer_ticks_per_bit_ + \
+	bt.timer_ticks_per_bit_max_ = bt.TIM_ticks_per_bit_ + \
 		bt.delta_timer_ticks_per_bit_;
-	bt.start_bit_ticks_ = 3 * bt.timer_ticks_per_bit_;
+	bt.start_bit_ticks_ = 3 * bt.TIM_ticks_per_bit_;
 	bt.start_bit_ticks_min_ = bt.start_bit_ticks_ - \
 		bt.delta_timer_ticks_per_bit_;
 	bt.start_bit_ticks_max_ = bt.start_bit_ticks_ + \
@@ -49,9 +49,9 @@ data_full_msg init_data_struct()
 uint8_t Add_signal_to_sequence(bit_time* bt, uint16_t* buffer, timer_receive_sequence *sequence, data_full_msg * data)
 {
 	if (sequence->sequence_iterator_ == MAX_SEQUENCE_ITERATOR) return 1;
-	sequence->receive_sequence_[sequence->sequence_iterator_] = buffer[0];
+	sequence->TIM_ticks_sequence_[sequence->sequence_iterator_] = buffer[0];
 	++sequence->sequence_iterator_;
-	sequence->receive_sequence_[sequence->sequence_iterator_] = buffer[1];
+	sequence->TIM_ticks_sequence_[sequence->sequence_iterator_] = buffer[1];
 	++sequence->sequence_iterator_;
 	Convert_sequence_find_data_length(bt, sequence, data);
 	if (sequence->sequence_iterator_ == MAX_SEQUENCE_ITERATOR) sequence->sequence_iterator_ = 0; // обнуление последовательности
@@ -64,29 +64,32 @@ uint8_t Add_signal_to_sequence(bit_time* bt, uint16_t* buffer, timer_receive_seq
 // разворот битов из LSB в MSB
 uint8_t Convert_sequence_find_data_length(bit_time* bt, timer_receive_sequence* tim_seq, data_full_msg* data )
 {
-	static uint8_t start_ = 0;
-	static uint16_t buffer_ = { 0, };  // буфер для заполнения 6 битными значениями
-	static uint8_t buffer_iterator_ = 0;
-	static uint8_t word_buffer_ = 0; // заполняется по 4 бита, для учета нужен итератор
-	static uint8_t halfword_iterator_ = 0;
+	// предпологается что таймер принял 6 + 6 бит
+	// TODO: проверка на шум если длительность много меньше длительности бита - отсекаем
+	uint8_t start_ = 0;
+	uint16_t buffer_ = { 0, };  // буфер для заполнения 6 битными значениями
+	uint8_t buffer_iterator_ = 0;
+	uint8_t word_buffer_ = 0; // заполняется по 4 бита, для учета нужен итератор
+	uint8_t halfword_iterator_ = 0;
 	uint8_t count_1 = 0, count_0 = 0;
 	if (start_ == tim_seq->sequence_iterator_)return 0;
 	// поиск
 	for (uint8_t i = start_; i < tim_seq->sequence_iterator_; i = i + 2) {//TODO: что если sequence_iterator нечетный
 
 		// разделяю на длительность бита (в отсчетах таймера)
-		count_1 = tim_seq->receive_sequence_[i] / bt->timer_ticks_per_bit_;  // TODO: 16 BIT TO 8 BIT
-		if (tim_seq->receive_sequence_[i] % bt->timer_ticks_per_bit_ > \
-			bt->timer_ticks_per_bit_min_ * count_1) ++count_1; // проверяю остаток
-		count_0 = tim_seq->receive_sequence_[i + 1] / bt->timer_ticks_per_bit_;
-		if (tim_seq->receive_sequence_[i + 1] % bt->timer_ticks_per_bit_ > \
-			bt->timer_ticks_per_bit_min_ * count_0) ++count_0;
+		count_1 = tim_seq->TIM_ticks_sequence_[i] / bt->TIM_ticks_per_bit_;  // TODO: 16 BIT TO 8 BIT
+		if (tim_seq->TIM_ticks_sequence_[i] / bt->TIM_ticks_per_bit_min_ >  count_1) ++count_1; // проверяю остаток длительность 
+		// "1" не должна быть большой
+		count_0 = tim_seq->TIM_ticks_sequence_[i + 1] / bt->TIM_ticks_per_bit_;
+		if (tim_seq->TIM_ticks_sequence_[i + 1] % bt->TIM_ticks_per_bit_min_ > count_0) ++count_0;
 
 		// перевожу из "1" и "0" в слово
 		// считаю что данные приходят младшим битом вперед
 		for (uint8_t y = 0; y < count_1; ++y) {
+
 			buffer_ |= 1 << buffer_iterator_;
 			++buffer_iterator_; // до этого момента buffer_iterator не должен быть больше 6
+
 			if (buffer_iterator_ == 6) {
 				if (halfword_iterator_ == 0) {
 					word_buffer_ = 0;
@@ -100,7 +103,9 @@ uint8_t Convert_sequence_find_data_length(bit_time* bt, timer_receive_sequence* 
 				buffer_ = 0;
 			}
 		}
+
 		if (buffer_iterator_ + count_0 >= 6) {
+
 			if (halfword_iterator_ == 0) {
 				word_buffer_ = Convert_6to4(buffer_);
 			}
