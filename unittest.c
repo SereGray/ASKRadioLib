1b4c79a049ba632c5fb4dc5e1a45945fe1358058
sochnevs@mail.ru <sochnevs@mail.ru>
2022-02-11 06:04:32 +0300

    add 2 test

--- unittest.c
+++ unittest.c
@@ -3,29 +3,58 @@
 
 //#include<stdio.h>
 #include "unity.h"
-#include "soft_rf.h"
+#include "soft_rf_internals.h"
 
 void setUp(void);
 void tearDown(void);
+void test_FunctionUnderTest_should_ReturnFive(void);
+int FunctionUnderTest();
+
+void test_convert_6to4(void);
+void test_convert_4to6(void);
+
 
 int main(void)
 {
-    //char test[] = "hello world ";
-    //printf("%s%d",test,10);
+    char test[] = "soft_rf_internals unit testing\n";
+    printf("%s",test);
     int a = 1;
     TEST_ASSERT(a == 1); //this one will pass
     //TEST_ASSERT(a == 2); //this one will fail
+    test_FunctionUnderTest_should_ReturnFive();
+    test_convert_6to4();
+    test_convert_4to6();
     getchar();
 }
 
-void setUp(void)
+void test_convert_6to4(void) 
 {
-
+    TEST_ASSERT_EQUAL_INT(15, convert_6to4(0x34));
+    TEST_ASSERT_EQUAL_INT(0, convert_6to4(0xd));
+    TEST_ASSERT_EQUAL_INT(4, convert_6to4(0x16));
 }
-void tearDown(void)
+
+void test_convert_4to6(void)
 {
-
+    TEST_ASSERT_EQUAL_INT(0x34, convert_4to6(15));
+    TEST_ASSERT_EQUAL_INT(0xd, convert_4to6(0));
+    TEST_ASSERT_EQUAL_INT(0x16, convert_4to6(4));
 }
+
+
+void setUp(void) {}
+void tearDown(void) {}
+
+int FunctionUnderTest()
+{
+    return 5;
+}
+
+void test_FunctionUnderTest_should_ReturnFive(void) {
+    TEST_ASSERT_EQUAL_INT(5, FunctionUnderTest());
+    TEST_ASSERT_EQUAL_INT(5, FunctionUnderTest()); //twice even!
+}
+
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
